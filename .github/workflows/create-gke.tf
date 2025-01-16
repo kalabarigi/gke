9527c08b4684a2b03dc9da4b4a04025d01935368
kalabarigi <82603974+kalabarigi@users.noreply.github.com>
2025-01-16 16:57:00 +0000

    Update create-gke.tf

--- .github/workflows/create-gke.tf
+++ .github/workflows/create-gke.tf
@@ -18,19 +18,19 @@ variable "gke_num_nodes" {
 
 # GKE cluster
 data "google_container_engine_versions" "gke_version" {
-  location = var.region
+  location = europe-west1
   version_prefix = "1.27."
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
-  location = var.region
+  name     = "tcs-lab-447919-gke"
+  location = europe-west1
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 1
+  initial_node_count       = 2
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
@@ -38,12 +38,12 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
+  name       = tcs-gke2
+  location   = europe-west1
+  cluster    = tcs-gke2
   
   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
-  node_count = var.gke_num_nodes
+  node_count = 2
 
   node_config {
     oauth_scopes = [
@@ -52,12 +52,12 @@ resource "google_container_node_pool" "primary_nodes" {
     ]
 
     labels = {
-      env = var.project_id
+      env = tcs-lab-447919
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.project_id}-gke"]
+    machine_type = "e2-micro"
+    tags         = ["gke-node", "tcs-lab-447919-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
@@ -65,21 +65,3 @@ resource "google_container_node_pool" "primary_nodes" {
 }
 
 
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
